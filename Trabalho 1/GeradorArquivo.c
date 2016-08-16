57744061c6020d946bbfcdea0a435d6a7c6949da
Éwerton Cercal <ewerton.cercal@gmail.com>
2016-08-16 11:39:57 -0300

    Modificada estrutura do arquivo.
    Modifcada função main(). Agora ela possui um menu para escolher se o
    usuário deseja gravar registros ou pesquisá-los. Além disso, no próprio
    início do main(), é verificada a existência do arquivo .bin onde serão
    gravados os dados. Caso não exista, ele é criado pela primeira vez.

--- Trabalho 1/GeradorArquivo.c
+++ Trabalho 1/GeradorArquivo.c
@@ -1,28 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <locale.h>
+#include <unistd.h>
 
-#define MSG_ERRO_ABERTURA "Quantidade inválida de parâmetros. Abortando execução do programa."
-
-main (int argc, char * argv[])
-
+int CriaArquivo(char *nome, char *modo)
 {
+
     FILE *fp;
 
-    setlocale(LC_ALL, "");  //Com essa função a acentuação e caracteres especiais aparecem em tela conforme a localidade do sistema.
-
-    if(argc < 2)    //Verifica se foi passado o número mínimo de argumentos.
-    {
-        printf(MSG_ERRO_ABERTURA);  //"Quantidade inválida de parâmetros. Abortando execução do programa."
-        exit(0);
-    }
-
-    if (( fp  = fopen(argv[1],  argv[2])) == NULL) //A primeira posição do vetor sinaliza o nome do arquivo, enquanto que a segunda informa qual o modo de abertura do mesmo.
+    if ((fp  = fopen(nome,  modo)) == NULL)   //A primeira posição do vetor sinaliza o nome do arquivo, enquanto que a segunda informa qual o modo de abertura do mesmo.
     {
         printf ("Erro  na  abertura  do  arquivo.");
         exit (0);
     }
 
     fclose(fp);
+
+    return 0;
+
+}
+
+int GravaRegistros()
+{
+
+    return 0;
+
+}
+
+int PesquisaRegistros()
+{
+
+    return 0;
+
+}
+
+int main (void)
+
+{
+
+    FILE *file;
+    char nome_arq[] = "golias.bin";
+    int escolha = 0;
+
+    setlocale(LC_ALL, "");  //Com essa função a acentuação e caracteres especiais aparecem em tela conforme a localidade do sistema.
+
+    if ((file  = fopen(nome_arq, "r+b")) != NULL)   //Verifica se o arquivo existe.
+    {
+        fclose(file);
+    }
+    else
+    {
+       CriaArquivo(nome_arq, "w+b");    //Aplicar tratamento para verificar se o arquivo já existe.
+    }
+
+    do {
+
+        do {
+
+            system("clear || cls"); //"Limpa" os dados em tela.
+
+            printf("Selecione a operação que deseja realizar:\n");
+            printf("1 - Realizar gravações de cadastros de alunos.;\n");
+            printf("2 - Realizar pesquisa de cadastros de alunos.\n");
+            printf("3 - Finalizar programa.\n");
+            printf("Sua escolha: ");
+            scanf("%d", &escolha);
+            getchar();
+
+        } while (escolha > 3 || escolha < 1);
+
+        system("clear || cls");
+
+        switch (escolha) {
+            case 3: break;  //Finalizar
+            case 1: GravaRegistros();
+                break;
+            case 2: PesquisaRegistros();
+                break;
+        }
+
+    } while (escolha != 3);
+
+    return 0;
 
 }
